b1fb3d9bd86349a616609020f24df894bf1991f1
Timothée Ravier <timothee.ravier@ssi.gouv.fr>
2019-10-23 14:36:33 +0200

    Enable exec for /tmp & /var/tmp tmpfs mounts
    Signed-off-by: Timothée Ravier <timothee.ravier@ssi.gouv.fr>

--- run.sh
+++ run.sh
@@ -2,8 +2,8 @@
 
 podman run --rm --tty --interactive \
     --volume "${PWD}:/mnt" \
-    --tmpfs '/tmp' \
-    --tmpfs '/var/tmp' \
+    --tmpfs '/tmp:exec' \
+    --tmpfs '/var/tmp:exec' \
     --workdir '/mnt' \
     'gentoo/hardened' \
     bash
